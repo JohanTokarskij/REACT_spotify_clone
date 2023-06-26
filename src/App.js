d639a25b3cafe688e19d076e8a9b8cec7add539d
Johan <johantokarskij@yahoo.se>
2023-06-26 16:39:42 +0200

    26 june, final commit for the day

--- src/App.js
+++ src/App.js
@@ -1,9 +1,40 @@
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Login from './Login';
+import { getTokenFromUrl } from './spotify';
+import SpotifyWebApi from 'spotify-web-api-js';
 
-function App() {
+const spotify = new SpotifyWebApi()
+
+const App = () => {
+  const [token, setToken] = useState(null)
+
+
+  useEffect(() => {
+    const hash = getTokenFromUrl()
+    window.location.hash = ''
+    const _token = hash.access_token
+
+    if (_token) {
+      setToken(_token)
+      spotify.setAccessToken(_token)
+      spotify.getMe().then(user => {
+        console.log('Welcome back!', user);
+      })
+    }
+  }, [])
+
+
   return (
     <div className="App">
-      <h1>Hello React!</h1>
+      {
+        token?
+          <>
+            <h1>You are logged in!</h1>
+            <button onClick={()=>setToken('')}>Logout</button>
+          </> : <Login />
+      }
+      
     </div>
   );
 }
